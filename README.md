e843adf683d93df433ba75612ef0c38cde411a41
Muah <muah003@gmail.com>
2017-12-12 10:39:41 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# vue-bounty
+# vue-bounty (Beta)
 https://github.com/coderitual/bounty
+
+## Usage
+
+```vue
+<bounty :value="100"></bounty>
+```
---
+++ src/bounty.vue
@@ -0,0 +1,35 @@
+<template>
+    <div/>
+</template>
+
+<script>
+import bounty from 'bounty'
+
+export default {
+    props: ['value'],
+    mounted() {
+        this.init(this.value, 0)
+    },
+    methods: {
+        init(val, old) {
+            return bounty({
+                el: this.$el,
+                value: val,
+                initialValue: old,
+                lineHeight: 1,
+                letterSpacing: 1,
+                animationDelay: 100,
+                letterAnimationDelay: 100
+            })
+        }
+    },
+    watch: {
+        value(val, oldVal) {
+            setTimeout(() => {
+                this.init(val, oldVal || 0)
+            }, 100)
+        }
+    },
+    render() {}
+}
+</script>
